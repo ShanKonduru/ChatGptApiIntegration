dbeed6e72afffa1eac1913300d7ce50333b84b2a
Shan Konduru <ShanKonduru@users.noreply.github.com>
2023-01-23 23:53:02 +0530

    You exceeded your current quota

--- Program.cs
+++ Program.cs
@@ -2,14 +2,14 @@
 
 if (args.Length > 0) {
     HttpClient client = new HttpClient ();
-    client.DefaultRequestHeaders.Add ("Content-Type", "application/json");
-    client.DefaultRequestHeaders.Add ("Authorization", "Bearer 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");
-    var content = new StringContent ("{\"model\": \"text-davinci-001\",\"prompt\":\"" + args[0] + "\",\"temperature\": 1,\"max_tokens\": 100,\"top_p\": 1,\"frequency_penalty\": 0.0,\"presence_penalty\": 0.0}",
+    // client.DefaultRequestHeaders.Add ("Content-Type", "application/json");
+    client.DefaultRequestHeaders.Add ("Authorization", "Bearer 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");
+    var content = new StringContent ("{\"model\": \"text-davinci-003\",\"prompt\":\"" + args[0] + "\",\"temperature\": 1,\"max_tokens\": 100,\"top_p\": 1,\"frequency_penalty\": 0.0,\"presence_penalty\": 0.0}",
         Encoding.UTF8, "application/json");
     HttpResponseMessage response = await client.PostAsync ("https://api.openai.com/v1/completions", content);
     Task<string> responseText = response.Content.ReadAsStringAsync();
 
-    Console.WriteLine(responseText.ToString());
+    Console.WriteLine(responseText.Result);
 } else {
     Console.WriteLine ("need at least one question to be asked");
 }
