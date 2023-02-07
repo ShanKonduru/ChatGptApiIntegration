f7b125b2f2ff4ea8e1f280c99c3ef944d11090b6
Shan Konduru <ShanKonduru@users.noreply.github.com>
2023-02-07 10:12:50 +0530

    base code

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 if (args.Length > 0) {
     HttpClient client = new HttpClient ();
     // client.DefaultRequestHeaders.Add ("Content-Type", "application/json");
-    client.DefaultRequestHeaders.Add ("Authorization", "Bearer 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");
+    client.DefaultRequestHeaders.Add ("Authorization", "Bearer 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");
     var content = new StringContent ("{\"model\": \"text-davinci-003\",\"prompt\":\"" + args[0] + "\",\"temperature\": 1,\"max_tokens\": 100,\"top_p\": 1,\"frequency_penalty\": 0.0,\"presence_penalty\": 0.0}",
         Encoding.UTF8, "application/json");
     HttpResponseMessage response = await client.PostAsync ("https://api.openai.com/v1/completions", content);
